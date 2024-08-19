590070406ba3fbc33f26de1b1fe3e8f9532c3850
Emilio Camacho <khavoks@gmail.com>
2024-08-19 18:06:32 -0500

    Corrected multiple Rules

--- main.tf
+++ main.tf
@@ -16,17 +16,50 @@ resource "random_string" "resource_code" {
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_string.resource_code.result}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  public_network_access_enabled = true
-  min_tls_version = "TLS1_2"
+  name                            = "tfstate${random_string.resource_code.result}"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = "GRS"
+  public_network_access_enabled   = true
+  min_tls_version                 = "TLS1_2"
   allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = false
 
   tags = {
-    environment = "staging"  
+    environment = "staging"
+  }
+
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    hour_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    minute_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+  sas_policy {
+    expiration_period = "90.00:00:00"
+    expiration_action = "Log"
+  }
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
   }
 }
 
@@ -34,4 +67,23 @@ resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "blob"
+}
+
+resource "azurerm_log_analytics_workspace" "analytics_workspace_ok" {
+  name                = "exampleworkspace"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+
+resource "azurerm_log_analytics_storage_insights" "analytics_storage_insights_ok" {
+  name                = "example-storageinsightconfig"
+  resource_group_name = azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.analytics_workspace_ok.id
+
+  storage_account_id   = azurerm_storage_account.tfstate.id
+  storage_account_key  = azurerm_storage_account.tfstate.primary_access_key
+  blob_container_names = ["tfstate"]
 }
