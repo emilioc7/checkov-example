a47c858fd34ff558f677cac05236d80f9509ed80
Emilio Camacho <khavoks@gmail.com>
2024-08-19 19:22:14 -0500

    Check if rules shows up again

--- main.tf
+++ main.tf
@@ -103,3 +103,54 @@ resource "azurerm_private_endpoint" "example" {
     subresource_names              = ["blob"]
   }
 }
+
+
+
+
+resource "azurerm_storage_account" "new_storage_emc" {
+  name                            = "newstorage{random_string.resource_code.result}"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = "GRS"
+  public_network_access_enabled   = false
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = false
+
+  tags = {
+    environment = "testing"
+  }
+
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    hour_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    minute_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+  sas_policy {
+    expiration_period = "90.00:00:00"
+    expiration_action = "Log"
+  }
+
+  blob_properties {
+    delete_retention_policy {
+      days = 10
+    }
+  }
+}
